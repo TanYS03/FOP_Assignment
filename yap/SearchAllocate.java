dea18cf416536a85e4de1a22ca12ddbd203128f2
Weili <weili.yiap@gmail.com>
2023-01-06 11:04:35 +0800

    hey

---
+++ yap/SearchAllocate.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class SearchAllocate {
+    public static void main(String[] args){
+        StringBuilder str = getTextFromFile("C:\\Users\\User\\Downloads\\extracted_log");
+        System.out.println(str);
+    }
+
+    public static StringBuilder getTextFromFile(String filePath) {
+        StringBuilder builder = new StringBuilder(); 
+            try (BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+                String line = reader.readLine();
+                    while(line != null)
+                    {
+                        builder.append(line);
+                        builder.append(System.getProperty("line.separator"));
+                        line = reader.readLine();
+                    }
+           }catch (FileNotFoundException ex) {System.out.println("File not found");}
+            catch (IOException e){}
+        
+        return builder;
+    }
+}
+   
+
