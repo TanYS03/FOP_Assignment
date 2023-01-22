92a7fde831a178a56081c2ba552fea8cf6f29056
YI SHAN TAN <22004867@siswa.um.edu.my>
2023-01-22 22:30:02 +0800

    delete file

--- Tester.java
+++
@@ -1,8 +0,0 @@
-public class Tester {
-    public static void main(String[ ] args){
-        System.out.println("Hello world");
-        System.out.println("i really dont know what to do guys.");
-        System.out.println("hey");
-    }
-
-} 
